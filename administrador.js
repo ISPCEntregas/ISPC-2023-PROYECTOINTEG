10905bd0f2292255a68a938fb9888933e29f2be6
Emiliano R Gillini <emigillini@hotmail.com>
2023-10-17 10:53:23 -0300

    sprint2

--- administrador.js
+++ administrador.js
@@ -1,47 +1,59 @@
+function cargarZapatillas() {
+    fetch('http://127.0.0.1:5000/zapatillas')
+        .then(response => response.json())
+        .then(data => {
+            const zapatillaList = document.getElementById('zapatilla-list');
+            zapatillaList.innerHTML = '';  // Limpiar la lista
 
-      function cargarZapatillas() {
-        $.get('/zapatillas', function (data) {
-            const zapatillaList = $('#zapatilla-list');
-            zapatillaList.empty(); 
-
-            data.forEach(function (zapatilla) {
-                const listItem = $('<li></li>');
-                listItem.text(`Nombre: ${zapatilla.nombre}, Marca: ${zapatilla.marca}, Talla: ${zapatilla.talla}, Precio: ${zapatilla.precio}, Categoría: ${zapatilla.categoria}`);
-                zapatillaList.append(listItem);
+            data.forEach(zapatilla => {
+                const listItem = document.createElement('li');
+                listItem.textContent = `Nombre: ${zapatilla.nombre}, Marca: ${zapatilla.marca}, Talla: ${zapatilla.talla}, Precio: ${zapatilla.precio}, Categoría: ${zapatilla.categoria}`;
+                zapatillaList.appendChild(listItem);
             });
+        })
+        .catch(error => {
+            console.error('Error:', error);
         });
-    }
+}
 
-   
-    $('#crear-zapatilla-form').submit(function (e) {
-        e.preventDefault();
+document.getElementById('crear-zapatilla-form').addEventListener('submit', function (e) {
+    e.preventDefault();
 
-        const nombre = $('#nombre').val();
-        const marcaId = parseInt($('#marca_id').val());
-        const talla = parseFloat($('#talla').val());
-        const precio = parseFloat($('#precio').val());
-        const categoriaId = parseInt($('#categoria_id').val());
+    const nombre = document.getElementById('nombre').value;
+    const marcaId = parseInt(document.getElementById('marca_id').value);
+    const talla = parseFloat(document.getElementById('talla').value);
+    const precio = parseFloat(document.getElementById('precio').value);
+    const categoriaId = parseInt(document.getElementById('categoria_id').value);
 
-        const nuevaZapatilla = {
-            nombre: nombre,
-            marca_id: marcaId,
-            talla: talla,
-            precio: precio,
-            categoria_id: categoriaId
-        };
+    const nuevaZapatilla = {
+        nombre: nombre,
+        marca_id: marcaId,
+        talla: talla,
+        precio: precio,
+        categoria_id: categoriaId
+    };
 
-        $.post('/zapatillas', JSON.stringify(nuevaZapatilla), function (data) {
-            alert(data);
-            cargarZapatillas(); 
-        });
-
-      
-        $('#nombre').val('');
-        $('#marca_id').val('');
-        $('#talla').val('');
-        $('#precio').val('');
-        $('#categoria_id').val('');
+    fetch('http://127.0.0.1:5000/zapatillas', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(nuevaZapatilla)
+    })
+    .then(response => response.text())
+    .then(data => {
+        alert(data);
+        cargarZapatillas();
+    })
+    .catch(error => {
+        console.error('Error:', error);
     });
 
- 
-    cargarZapatillas();
+    document.getElementById('nombre').value = '';
+    document.getElementById('marca_id').value = '';
+    document.getElementById('talla').value = '';
+    document.getElementById('precio').value = '';
+    document.getElementById('categoria_id').value = '';
+});
+
+cargarZapatillas();  // Cargar la lista inicial
